1f7fa974c7a1fd657e68b1ff73cece60210ecacb
AMBITIOUSAWC <abagnawclem@gmail.com>
2025-09-28 16:02:09 +0100

    Add wsgi.py for successful deployment on Render

--- nexus_backend/wsgi.py
+++ nexus_backend/wsgi.py
@@ -1,16 +1,7 @@
-"""
-WSGI config for nexus_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+from django.core.asgi import get_asgi_application
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus_backend.settings')
+application = get_asgi_application()
 
-application = get_wsgi_application()
